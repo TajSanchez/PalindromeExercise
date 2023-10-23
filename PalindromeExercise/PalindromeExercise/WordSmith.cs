e34d839f3e82e5775d0ee535b092fafbc3e8cb22
Taj <tajmarie92@gmail.com>
2023-10-23 14:06:11 -0500

    Completed Exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+
+        public bool IsAPalindrome(string word)
+        { 
+            //reverse the word to see if it is the same after reversed
+            var reversed = word.Reverse();
+          if (reversed != word) 
+            {   
+                 
+                return word.SequenceEqual(word.Reverse());
+               //when I return false, it does not pass 4 out of the 7 tests I am going to look more into this
+            }
+
+            return true;
+                
+                //bool.Parse(word);
+            
+
+            //compare with the original
+            //return whether true/false
+           
+            
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,25 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hey", false)]
+        [InlineData("civic", true)]
+        [InlineData("noon", true)]
+        [InlineData("howdy", false)]
+        [InlineData("hannah", true)]
+        [InlineData("brother",false)]
 
+        public void Test1(string name, bool expected)
+        {
+            //Arrange
+            var pal = new WordSmith();
+
+            //Act
+            var actual = pal.IsAPalindrome(name);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
